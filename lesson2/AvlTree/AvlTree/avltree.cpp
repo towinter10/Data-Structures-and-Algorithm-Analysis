b41ee7c57143c4b01357f4516141a1dfd7f8605d
towinter10 <291108086@qq.com>
2020-12-04 15:08:36 +0800

    更新寻找不平衡节点算法

--- lesson2/AvlTree/AvlTree/avltree.cpp
+++ lesson2/AvlTree/AvlTree/avltree.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include"avltree.h"
+#include<stack>
+#include<vector>
 using namespace std;
 
 template<typename T>
@@ -12,6 +14,18 @@ template<typename T>
 AvlTree<T>::~AvlTree()
 {
 	Clear(root);
+}
+
+template<typename T>
+void AvlTree<T>::Clear(BiNode<T>* n)
+{
+	if (n)
+	{
+		Clear(n->LChild);
+		Clear(n->RChild);
+		delete n;
+	}
+
 }
 
 template<typename T>
@@ -61,17 +75,40 @@ void AvlTree<T>::AllBF(BiNode<T>* nood)
 }
 
 template<typename T>
-void AvlTree<T>::BFTwo(BiNode<T>* nood, BiNode<T>* tmp)
+BiNode<T>* AvlTree<T>::BFTwo(BiNode<T>* node)
 {
-	if (nood != nullptr)
+	stack<BiNode<T>*> s;
+	vector<BiNode<T>*> t;
+	BiNode<T>* tmp1 = node;
+	s.push(node);
+	while (s.empty() != true)
 	{
-		if (nood->bf == 2 || nood->bf == -2)
+		tmp1 = s.top();
+		t.push_back(tmp1);
+		s.pop();
+		if (tmp1->RChild != nullptr)
 		{
-			tmp = nood;
+			s.push(tmp1->RChild);
 		}
-		BFTwo(nood->LChild, tmp);
-		BFTwo(nood->RChild, tmp);
+		if (tmp1->LChild != nullptr)
+		{
+			s.push(tmp1->LChild);
+		}
 	}
+	BiNode<T>* tmp2 = new BiNode<T>;
+	while (t.empty()!= true)
+	{
+		tmp2 = t.back();
+		if (tmp2->bf == 2 || tmp2->bf == -2)
+		{
+			break;
+		}
+		else
+		{
+			t.pop_back();
+		}
+	}
+	return tmp2;
 }
 
 template<typename T>
@@ -103,18 +140,31 @@ void AvlTree<T>::LL(BiNode<T>* tmp)
 {
 	if (tmp == root)
 	{
+		BiNode<T>* L = tmp->LChild;
+		tmp->LChild = L->RChild;
+		L->RChild = tmp;
+		root = L;
+	}
+	else
+	{
+		BiNode<T>* L = tmp->LChild;
+		BiNode<T>* P = new BiNode<T>;
+		AvlTree<T>::BFTwoParent(root, tmp, P);
+		tmp->LChild = L->RChild;
+		L->RChild = tmp;
+		P->LChild = L;
 
 	}
 }
 
-template<typename T>
-void AvlTree<T>::RR(BiNode<T>* tmp);
-
-template<typename T>
-void AvlTree<T>::LR(BiNode<T>* tmp);
-
-template<typename T>
-void AvlTree<T>::RL(BiNode<T>* tmp);
+//template<typename T>
+//void AvlTree<T>::RR(BiNode<T>* tmp);
+//
+//template<typename T>
+//void AvlTree<T>::LR(BiNode<T>* tmp);
+//
+//template<typename T>
+//void AvlTree<T>::RL(BiNode<T>* tmp);
 
 
 template<typename T>
@@ -124,8 +174,72 @@ void AvlTree<T>::Insert(const T& Data)
 	{
 		root = new BiNode<T>;
 		root->Data = Data;
-		root->LChild = Root->RChild = nullptr;
+		root->LChild = root->RChild = nullptr;
 		root->bf = 0;
 	}
+	else
+	{
+		BiNode<T>* node = new BiNode<T>;
+		BiNode<T>* tmp = root;
+		while (1)
+		{
+			if (Data < tmp->Data)
+			{
+				if (tmp->LChild == nullptr)
+				{
+					node->Data = Data;
+					node->LChild = node->RChild = nullptr;
+					tmp->LChild = node;
+					break;
+				}
+				else
+				{
+					tmp = tmp->LChild;
+				}
+			}
+			else
+			{
+				if (tmp->RChild == nullptr)
+				{
+					node->Data = Data;
+					node->LChild = node->RChild = nullptr;
+					tmp->RChild = node;
+					break;
+				}
+				else
+				{
+					tmp = tmp->RChild;
+				}
+			}
+		}
+		AllBF(root);
+		BiNode<T>* nodet = BFTwo(root);
+		//BFTwo(root, nodet);
+		if (nodet->bf == 2)
+		{
+			LL(nodet);
+		}
 
+	}
 }
+
+
+int main()
+{
+	AvlTree<int> t1;
+	t1.Insert(10);
+	t1.Insert(15);
+	t1.Insert(5);
+	t1.Insert(3);
+	t1.Insert(7);
+	t1.Insert(13);
+	t1.Insert(17);
+	t1.Insert(2);
+	t1.Insert(1);
+	return  0;
+}
+
+//AllBF(root);
+//BiNode<T>* nodet = new BiNode<T>;
+//BFTwo(root, nodet);
+//LL(nodet);
